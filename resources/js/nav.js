4054222cae45453f12f70789c1963d7dbaa16486
Benjamin Wang <42283881+benwang2@users.noreply.github.com>
2021-03-17 15:41:21 -0400

    Added resource files via upload

---
+++ resources/js/nav.js
@@ -0,0 +1,36 @@
+let sections = [];
+let navbar = null;
+
+function addClass(name) {
+    var element, name, arr;
+    element = document.getElementById("nav-top");
+    arr = element.className.split(" ");
+    if (arr.indexOf(name) == -1) {
+        element.className += " " + name;
+    }
+}
+
+function rmvClass(name){
+    var element, name, arr;
+    element = document.getElementById("nav-top");
+    arr = element.className.split(" ");
+    if (arr.indexOf(name) >= 0) {
+        arr.splice(arr.indexOf(name), 1)
+        element.className = arr.join(" ")
+    }
+}
+
+window.onload = ()=>{
+    navbar = document.getElementById("nav-top")
+    sections = [document.getElementById("home"),document.getElementById("about"),document.getElementById("menu"),document.getElementById("contact")];
+}
+
+window.addEventListener("scroll",(evt)=>{
+    let scrollPosition = window.scrollY;
+    console.log(scrollPosition+" <= "+(sections[1].offsetTop - 61));
+    if (scrollPosition <= (sections[1].offsetTop - 61)){
+        addClass("opaque-black")
+    } else {
+        rmvClass("opaque-black")
+    }
+})
